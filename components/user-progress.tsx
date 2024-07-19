e065ca354283558af232fbb1b65e7efb3f3131b8
redfit <nagita.0724.0812@gmail.com>
2024-07-19 21:23:17 +0900

    Learn page and wrapper

---
+++ components/user-progress.tsx
@@ -0,0 +1,65 @@
+import Image from "next/image"
+import Link from "next/link"
+
+import { InfinityIcon } from "lucide-react"
+
+import { Button } from "@/components/ui/button"
+
+interface Props {
+  activeCourse: { title: string; imageSrc: string }
+  hearts: number
+  points: number
+  hasActiveSubscription: boolean
+}
+const UserProgress = ({
+  activeCourse,
+  hearts,
+  points,
+  hasActiveSubscription,
+}: Props) => {
+  return (
+    <div className="flex items-center justify-between gap-x-2 w-full">
+      <Link href="/courses">
+        <Button>
+          <Image
+            src={activeCourse.imageSrc}
+            alt={activeCourse.title}
+            className="rounded-md border"
+            height={32}
+            width={32}
+          />
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-orange-500">
+          <Image
+            src="/points.svg"
+            height={28}
+            width={28}
+            alt="points"
+            className="mr-2"
+          />
+          {points}
+        </Button>
+      </Link>
+      <Link href="/shop">
+        <Button variant="ghost" className="text-rose-500">
+          <Image
+            src="/heart.svg"
+            height={28}
+            width={28}
+            alt="heart"
+            className="mr-2"
+          />
+          {hasActiveSubscription ? (
+            <InfinityIcon className="h-4 w-4 stroke-3" />
+          ) : (
+            hearts
+          )}
+        </Button>
+      </Link>
+    </div>
+  )
+}
+
+export default UserProgress
