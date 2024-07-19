a36390717bbdf60f0583f1482ea6cd95922f9ead
redfit <nagita.0724.0812@gmail.com>
2024-07-19 21:59:30 +0900

    Drizzle

---
+++ db/drizzle.ts
@@ -0,0 +1,11 @@
+import { drizzle } from "drizzle-orm/neon-http"
+
+import { neon } from "@neondatabase/serverless"
+
+import * as schema from "@/db/schema"
+
+const sql = neon(process.env.DATABASE_URL!)
+// @ts-ignore
+const db = drizzle(sql, { schema })
+
+export default db
